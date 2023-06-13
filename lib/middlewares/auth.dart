1a0559e3ec0b020018c291a2a902228e58552d36
bryanbill <emobrya@gmail.com>
2023-06-13 11:14:54 +0300

    adds admin middleware

--- lib/middlewares/auth.dart
+++ lib/middlewares/auth.dart
@@ -16,9 +16,41 @@ class Auth extends Middleware {
 
       final jwt = verify(tkn);
 
-      print(jwt.payload['role']);
-
       if (jwt.payload['role'] == null) {
+        return RequestOrResponse(
+          response: Response.unauthorized({'message': 'Unauthorized'}),
+        );
+      }
+
+      return RequestOrResponse(
+          request: request..headers?['id'] = jwt.payload['id'].toString());
+    } catch (e) {
+      print(e);
+      return RequestOrResponse(
+        response: Response.unauthorized({'message': 'Unauthorized'}),
+      );
+    }
+  }
+}
+
+
+class Admin extends Middleware {
+  const Admin();
+
+  @override
+  Future<RequestOrResponse> handle(Request request) async {
+    try {
+      final tkn =
+      request.headers?['authorization']?.split('Bearer').last.trim();
+      if (tkn == null)
+        return RequestOrResponse(
+          response: Response.unauthorized({'message': 'Unauthorized'}),
+        );
+
+      final jwt = verify(tkn);
+
+
+      if (jwt.payload['role'] != 'admin') {
         return RequestOrResponse(
           response: Response.unauthorized({'message': 'Unauthorized'}),
         );
